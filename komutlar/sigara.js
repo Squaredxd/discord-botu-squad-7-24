1f8a340264c646ce62aaf37ac4388dabaa36ea88
Squaredxd <43114694+Squaredxd@users.noreply.github.com>
2018-09-13 17:59:27 +0300

    Update sigara.js

--- komutlar/sigara.js
+++ komutlar/sigara.js
@@ -5,7 +5,7 @@ exports.run = function(client, message) {
 
 	const embed = new Discord.RichEmbed()
 		.setDescription("**Sigara Sağlığa Zararlıdır -Solar**")
-		.setImage("https://imgyukle.com/i/S1qAj")
+		.setImage("https://i.imgyukle.com/2018/09/13/S1li1.jpg")
 	
 	message.channel.send(embed)
 };
