0b937e3e26e81e9fba37a02e30cb38fae09c57dc
Squaredxd <43114694+Squaredxd@users.noreply.github.com>
2018-09-13 18:25:44 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,13 +4,13 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
 
     const embed = new Discord.RichEmbed()
-        .setDescription("**YARDIM**")
-        .addField("Komutlar:",
-            "++anakomutlar: Ana komutları gösterir.",
-	     "++eğlencekomutları: Eğlence komutlarını gösterir.")
-
-	message.channel.send(embed)
-
+        .setDescription("**Yardım Menüsü**")
+        .setColor(0x00A385)
+        .addField("++eğlencekomutları:", "Eğlence Komutlarını Gösterir.", true)
+        .addField("++anakomutlar", "Ana Komutları Gösterir.", true)
+    
+    message.channel.send(embed)
+    
 };
 
 exports.conf = {
